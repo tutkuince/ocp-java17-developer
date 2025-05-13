67b53075f0ae0c05d63488004d77b01ef85a6996
Tutku Ince <tutku.ince@outlook.com>
2025-05-13 20:11:06 +0300

    comments

---
+++ src/main/java/com/incetutku/ocp/chapter01/class_structure/Comments.java
@@ -0,0 +1,17 @@
+package com.incetutku.ocp.chapter01.class_structure;
+
+/**
+ * Javadoc multiple-line comment
+ * @author Tutku Ince
+ */
+public class Comments {
+    // comment until end of line
+
+    /* Multiple
+     * line comment
+     */
+
+    /*
+     * // anteater
+     */
+}
