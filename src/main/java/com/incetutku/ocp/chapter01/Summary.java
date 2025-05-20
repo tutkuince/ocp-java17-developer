34414f6d6fbb178db9eeb5d4b11601dad6077565
Tutku Ince <tutku.ince@outlook.com>
2025-05-20 23:40:40 +0300

    Summary

---
+++ src/main/java/com/incetutku/ocp/chapter01/Summary.java
@@ -0,0 +1,16 @@
+package com.incetutku.ocp.chapter01;
+
+public class Summary {
+
+    /*
+    * Summary
+     * - Java begins program execution with a main() method.
+     * - Java code is organized into folders called packages.
+     * - The package java.lang is special in that it does not need to be imported.
+     * - For some class elements, order matters within the file.
+     *  1) The package statement comes first if present.
+     *  2) Then comes the class declaration
+     *  3) Fields and methods are allowed to be in any order within the class.
+     *
+    * */
+}
