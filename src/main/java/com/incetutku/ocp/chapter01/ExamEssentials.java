d50e6296dbf4dbbb89ed0a2982e72a6693528e2e
Tutku Ince <tutku.ince@outlook.com>
2025-05-21 20:40:05 +0300

    ExamEssentials

---
+++ src/main/java/com/incetutku/ocp/chapter01/ExamEssentials.java
@@ -0,0 +1,16 @@
+package com.incetutku.ocp.chapter01;
+
+public class ExamEssentials {
+    /*
+     * Exam Essentials
+     * - Be able to write code using a main() method.            --> public static void main(String[] args)
+     * - Understand the effect of using packages and imports.    --> Packages contain Java classes.
+     * - Be able to recognize a constructor.                     --> A constructor has the same name as the class.
+     * - Be able to identify legal and illegal declarations and initialization.
+     * - Understand how to create text blocks.                   --> A text block begins with """ on the first line.
+     * - Be able to use var correctly                            --> a var is used for a local variable.
+     * - Be able to determine where variables go into and out of scope
+     * - Know how to identify when an object is eligible for garbage collection.
+     *
+     * */
+}
