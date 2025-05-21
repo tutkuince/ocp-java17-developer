1d185a8be084b7e9cb3f4bf30608bd4fe33c78d6
Tutku Ince <tutku.ince@outlook.com>
2025-05-21 17:56:38 +0300

    Summary

--- src/main/java/com/incetutku/ocp/chapter01/Summary.java
+++ src/main/java/com/incetutku/ocp/chapter01/Summary.java
@@ -11,6 +11,24 @@ public class Summary {
      *  1) The package statement comes first if present.
      *  2) Then comes the class declaration
      *  3) Fields and methods are allowed to be in any order within the class.
-     *
+     * - Primitive types are the basic building blocks of Java types.
+     * - They are assembled into reference types.
+     * - Reference types can have methods and be assigned a null value.
+     * - Numeric literals are allowed to contain underscores (_) as long as they do not start or end the literal and are not next to decimal point.
+     * - Wrapper classes are reference types, and there is one for each primitive.
+     * - Text blocks allow creating a String on multiple lines using """.
+     * - Local variables must be specifically initialized before they can be used.
+     * - Local variable declarations may use the "var" keyword instead of the actual type.
+     * - When using var, the type is set once at compile time and does not change.
+     *  - Scope refers to that portion of code where a variable can be accessed.
+     * - There are three kinds of variables in Java, depending on their scope: instance variables, class variables, and local variables.
+     * - Instance variables are the non-static fields of your class.
+     * - Class variables are the static fields within a class
+     * - Local variables are declared within a constructor, method, or initializer block.
+     * - Constructors create Java objects.
+     * - A constructor is a method matching the class name and omitting the return type.
+     * - When an object is instantiated, fields and blocks of code are initialized first. Then the constructor is run.
+     * - Garbage collection is responsible for removing objects from memory when they can never be used again.
+     * - An object becomes eligible for garbage collection when there are no more references to it or its references have all gone out of scope.
     * */
 }
